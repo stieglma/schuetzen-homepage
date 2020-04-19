907a6f7065dda6ada08cfad052296ca39233f7ac
Thomas Stieglmaier <thomas.stieglmaier@ikor.de>
2020-04-19 22:15:19 +0200

    update rsync script

--- rsync-upload.sh
+++ rsync-upload.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+rm -rf output
+mkdir output
+pipenv run publish
 cd output
 rsync -rclpvh --delete -e ssh . stieglma@sagitta.uberspace.de:/var/www/virtual/stieglma/edelweiss-gaishofen.de
+
