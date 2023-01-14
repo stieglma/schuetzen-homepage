b2f9a14f5e63777810152e8acb62e19d10b79392
Thomas Stieglmaier <thomas@stieglmaier.me>
2023-01-14 13:47:57 +0100

    Delete rsync-upload.sh

--- rsync-upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf output
-mkdir output
-pipenv run publish
-cd output
-rsync -rclpvh --delete -e ssh . stieglma@sagitta.uberspace.de:/var/www/virtual/stieglma/edelweiss-gaishofen.de
-
